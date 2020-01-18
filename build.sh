36f28a0c210297e227c4872cef60ebcd966efbbd
Zhiming Qiu <zhiming.qiu@gmail.com>
2020-01-18 17:13:02 +0800

    Add JUnit task and a couple empty test cases

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 vendor/apache-ant-1.10.7/bin/ant -buildfile build.xml clean
-vendor/apache-ant-1.10.7/bin/ant -buildfile build.xml test-compile
+vendor/apache-ant-1.10.7/bin/ant -buildfile build.xml test
