9081e3db7531ad479802753285d5975a51b9025d
Zhiming Qiu <zhiming.qiu@gmail.com>
2020-01-18 15:52:16 +0800

    Set up ant as build system

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ class Main
 {
   public static void main(String[] args)
   {
-      
+    // test if distance-calculator.jar is loadable
+    Node n = new Node(false, 0, 0);
   }
 }
---
+++ test/TestUnit.java
@@ -0,0 +1,5 @@
+import src.Unit;
+
+public class TestUnit {
+
+}
