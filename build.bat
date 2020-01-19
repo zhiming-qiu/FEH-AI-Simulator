f516cf0a38ff6f084b9b281e37b73d3511fca666
zhiming-qiu <zhiming.qiu@gmail.com>
2020-01-19 15:07:47 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
 vendor/apache-ant-1.10.7/bin/ant -buildfile build.xml clean
-REM vendor/apache-ant-1.10.7/bin/ant -buildfile build.xml test
-vendor/apache-ant-1.10.7/bin/ant -buildfile build.xml checkstyle
+vendor/apache-ant-1.10.7/bin/ant -buildfile build.xml test
