4f238c5a84c2331e97af1fab559b15e1069bc82b
zhiming-qiu <zhiming.qiu@gmail.com>
2020-01-19 14:56:34 +0800

    Use JDK 10 on AppVeyors CI (#17)
    * Update appveyor.yml
    
    * Update build.bat
    
    * Update appveyor.yml
    
    * Update appveyor.yml
    
    * Update appveyor.yml
    
    * Update appveyor.yml
    
    * Update appveyor.yml
    
    * Update appveyor.yml
    
    * Update appveyor.yml
    
    * Update appveyor.yml

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
-REM vendor/apache-ant-1.10.7/bin/ant -buildfile build.xml clean
+vendor/apache-ant-1.10.7/bin/ant -buildfile build.xml clean
 REM vendor/apache-ant-1.10.7/bin/ant -buildfile build.xml test
-REM vendor/apache-ant-1.10.7/bin/ant -buildfile build.xml clean
 vendor/apache-ant-1.10.7/bin/ant -buildfile build.xml checkstyle
